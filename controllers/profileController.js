3edba4700bc66e7298888a82cf669bd9780fba0f
Hassan Alwayel <hsn.alwayel@gmail.com>
2020-09-02 18:57:03 +0300

    profile check user.id from jwt str, fetchP removed

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,17 +1,6 @@
 const Profile = require("../db/models/Profile");
 const User = require("../db/models/User");
 
-// Fetch
-exports.fetchProfile = async (profileId, next) => {
-  try {
-    const profile = await Profile.findByPk(profileId);
-    console.log(profile);
-    return profile;
-  } catch (error) {
-    next(error);
-  }
-};
-
 // List
 exports.profileList = async (req, res, next) => {
   try {
@@ -32,17 +21,9 @@ exports.profileList = async (req, res, next) => {
 // updateProfile
 exports.profileUpdate = async (req, res, next) => {
   try {
-    const { profile } = req;
-    // REVIEW: You don't need to check if `req.user` exists or not. If it doesn't exist jwt strategy won't allow it to reach the controller. I believe I've mentioned this before 🤔
-    if (req.user && req.user.id === profile.userId) {
-      // REVIEW: You de-structured req.profile above, why are you not using the de-structured one?
-      await req.profile.update(req.body);
-      res.status(204).end();
-    } else {
-      const err = new Error("Unauthorized");
-      err.status = 401;
-      next(err);
-    }
+    const profile = await Profile.findByPk(req.user.id);
+    await profile.update(req.body);
+    res.status(204).end();
   } catch (error) {
     next(error);
   }
--- routes/profiles.js
+++ routes/profiles.js
@@ -4,22 +4,8 @@ const passport = require("passport");
 const upload = require("../middleware/multer");
 const {
   profileUpdate,
-  fetchProfile,
   profileList,
 } = require("../controllers/profileController");
-
-// Param
-router.param("profileId", async (req, res, next, profileId) => {
-  const profile = await fetchProfile(profileId, next);
-  if (profile) {
-    req.profile = profile;
-    next();
-  } else {
-    const err = new Error("Profile Not Found");
-    err.status = 404;
-    next(err);
-  }
-});
 
 // List
 router.get("/", profileList);
@@ -27,7 +13,7 @@ router.get("/", profileList);
 // UpdateProfile
 // REVIEW: Do you need a profile ID? Every profile has a userId right? And since this route uses a jwt strategy, it receives `req.user`. So how can use `req.user` to update a profile? I want an answer to this in tomorrow's (2-Sept) standup meeting.
 router.put(
-  "/:profileId",
+  "/",
   passport.authenticate("jwt", { session: false }),
   upload.single("image"),
   profileUpdate
